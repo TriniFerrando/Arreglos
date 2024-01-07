683f33e5ddc98e670b5b0553c4480bbe24f65def
Trinidad Ferrando <triniferrando19@gmail.com>
2024-01-07 12:59:12 -0300

    Ordenar el arreglo por el método sort (mayor a menor)

--- src/EjemplosArreglos.java
+++ src/EjemplosArreglos.java
@@ -1,18 +1,28 @@
 import javax.lang.model.SourceVersion;
+import java.util.Arrays;
 
 public class EjemplosArreglos {
     public static void main(String[] args) {
         int[] numeros = new int[4]; //-1 para la última posición
+
+        numeros[0]=10;
+        numeros[1]=-6;
+        numeros[2]=2;
+        numeros[3]=56;
+
+        Arrays.sort(numeros);//los ordena de menor a mayor
 
         int i = numeros[0];
         int j = numeros[1];
         int k = numeros[2];
         int l = numeros[3]; // es igual a numero[numero.length -1]
 
-        System.out.println("l = " + l);
-        System.out.println("k = " + k);
-        System.out.println("j = " + j);
         System.out.println("i = " + i);
+        System.out.println("j = " + j);
+        System.out.println("k = " + k);
+        System.out.println("l = " + l);
+
+
 
 
     }
