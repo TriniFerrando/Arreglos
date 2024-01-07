f73c941a6fd2285f0c2e6273d2a3897cc2a4280e
Trinidad Ferrando <triniferrando19@gmail.com>
2024-01-07 12:32:45 -0300

    Inicio de arreglos

---
+++ src/EjemplosArreglos.java
@@ -0,0 +1,19 @@
+import javax.lang.model.SourceVersion;
+
+public class EjemplosArreglos {
+    public static void main(String[] args) {
+        int[] numeros = new int[4]; //-1 para la última posición
+
+        int i = numeros[0];
+        int j = numeros[1];
+        int k = numeros[2];
+        int l = numeros[3];
+
+        System.out.println("l = " + l);
+        System.out.println("k = " + k);
+        System.out.println("j = " + j);
+        System.out.println("i = " + i);
+
+
+    }
+}
