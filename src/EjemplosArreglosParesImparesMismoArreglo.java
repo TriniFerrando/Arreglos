85fb054fcb287dc63dbb59ae937cd7191c37df82
Trinidad Ferrando <triniferrando19@gmail.com>
2024-01-08 11:24:30 -0300

    Copiar pares e impares de un arreglo a otros arreglos segun que sean

--- src/EjemplosArreglosParesImparesMismoArreglo.java
+++ src/EjemplosArreglosParesImparesMismoArreglo.java
@@ -1,10 +1,12 @@
-//Copiar pares e impares de un arreglo de enteros y en sus respectivos arreglos
+//Copiar pares e impares de un arreglo a otros arreglos segun que sean
 
 import java.util.Scanner;
 
 public class EjemplosArreglosParesImparesMismoArreglo {
     public static void main(String[] args) {
-        int[] numeros = new int[6];
+        int[] numeros, pares, impares; //declaro los 3 arreglos
+
+        numeros = new int[6];          //Asigno valor al primer arreglo
         Scanner scanner= new Scanner(System.in);
 
         int largo= numeros.length;
@@ -14,5 +16,41 @@ public class EjemplosArreglosParesImparesMismoArreglo {
             numeros[i]= scanner.nextInt();
         }
 
+
+        int contPares=0;
+        int contImpares=0;
+        for(int i=0; i<largo; i++){
+            if(numeros[i] % 2 ==0){
+                contPares++;
+            }else{
+                contImpares++;
+            }
+
+        }
+
+        pares=new int[contPares];  //Asigno valor al segundo arreglo
+        impares= new int[contImpares]; //Asigno valor al tercer arreglo
+
+
+        int k=0;
+        int j=0;
+        for(int i=0; i<largo; i++){
+            if(numeros[i] % 2 ==0){
+                pares[k++]=numeros[i];
+            }else{
+                impares[j++]=numeros[i];
+            }
+
+        }
+
+        System.out.println("Pares:");
+        for(int i=0; i<pares.length ; i++){
+            System.out.println(pares[i]);
+        }
+        System.out.println("\n");
+        System.out.println("Impares:");
+        for(int i=0; i<impares.length ; i++){
+            System.out.println(impares[i]);
+        }
     }
 }
